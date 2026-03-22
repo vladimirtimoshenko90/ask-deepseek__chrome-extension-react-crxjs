faee0936ff48f3cfd2e85efa06ef89c1be8656ea
Vlad <vladimirtimoshenko90@gmail.com>
2026-03-22 15:17:27 +0300

    full-screen mode - db processing

--- src/content/FullscreenToggle/FullscreenToggle.tsx
+++ src/content/FullscreenToggle/FullscreenToggle.tsx
@@ -1,15 +1,26 @@
+import { useEffect, useState } from 'react';
+
 import { Switch } from '@mantine/core';
+import { storage } from '@/infrastructure/storage';
 import styles from './FullscreenToggle.module.scss';
-import { useState } from 'react';
 
 function FullscreenToggle() {
 	const [enabled, setEnabled] = useState(false);
+
+	useEffect(() => {
+		storage.getWideMode().then(setEnabled);
+	}, []);
+
+	const handleChange = async (value: boolean) => {
+		setEnabled(value);
+		await storage.setWideMode(value);
+	};
 
 	return (
 		<div className={styles['ads-fullscreen-toggle']}>
 			<Switch
 				checked={enabled}
-				onChange={(e) => setEnabled(e.currentTarget.checked)}
+				onChange={(e) => handleChange(e.currentTarget.checked)}
 				label="Full Screen"
 				size="sm"
 			/>
